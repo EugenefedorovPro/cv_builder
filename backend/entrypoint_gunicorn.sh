f2c59bbf6161d4ac95bb9ccbc8289ad88e265c34
eugene <eugene.proskulikov@gmail.com>
2025-05-23 13:58:52 +0300

    first commit

---
+++ backend/entrypoint_gunicorn.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+python manage.py makemigrations
+python manage.py migrate
+#python manage.py collectstatic --noinput
+
+## for development only
+python manage.py runserver 0.0.0.0:8002 --nothreading
+## alternative for development
+#gunicorn monitor_project.wsgi:application --bind 0.0.0.0:8002 --workers=2 --timeout=300 --reload
